b0d61b1408987edd93ef2edf1cac2475d14b7c62
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2021-10-29 01:03:06 +0100

    Day 3 part 2 working and some optimisations

--- src/aoc2019/d3/mod.rs
+++ src/aoc2019/d3/mod.rs
@@ -2,7 +2,7 @@ use crate::utils;
 use std::convert::From;
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::ops::RangeInclusive;
+use std::iter::Iterator;
 
 #[derive(Debug)]
 enum Travel {
@@ -47,17 +47,20 @@ pub fn solve(input1: String, _: String, _: &[String]) {
                 } else {
                     next
                 }
-            });
+            })
+            .unwrap();
 
-        println!("Nearest {:?}", nearest_intersection);
+        println!("Nearest {:?} with distance {}", nearest_intersection, nearest_intersection.0 + nearest_intersection.1);
 
         let it = get_time_distances(wire_1, &intersections);
         let it2 = get_time_distances(wire_2, &intersections);
 
-        it.iter()
-            .for_each(|(pos, distance)| {
-                println!("Total distance to {:?}: {}", pos, it2.get(pos).unwrap() + distance);
-            });
+        let fastest_intersection = it.iter()
+            .map(|(pos, distance)| (pos, it2.get(pos).unwrap() + distance))
+            .reduce(|last, next| if last.1 > next.1 { next } else { last })
+            .unwrap();
+
+        println!("Fastest {:?} with distance {}", fastest_intersection.0, fastest_intersection.1);
 }
 
 fn get_edges(path: &Vec<Travel>) -> (HashMap<i32, HashSet<i32>>, HashMap<i32, HashSet<i32>>) {
@@ -94,28 +97,26 @@ fn get_all_edge_data(path: &Vec<Travel>, intersections: &Vec<(i32, i32)>) -> (
 
         match t {
             Travel::Right(_)|Travel::Left(_) => {
-                update_crossings(y, &mut verticals, x + 1, current.0 - 1, |path_distance: i32, on: i32| {
+                let check_each = |path_distance: i32, on: i32| {
                     let pos = (on, y);
-                    if on == -1435 && y == 2474 {
-                        println!("FOUND! {} + {}", distance, path_distance);
-                    }
                     if intersections.contains(&pos) {
-                        time_distances.entry(pos).or_insert(distance + path_distance);
-                        println!("Found intersection after {} at {:?}", distance + path_distance, (on, y));
+                        time_distances.entry(pos).or_insert_with(|| {
+                            distance + path_distance
+                        });
                     }
-                });
+                };
+                update_crossings(y, &mut verticals, x, current.0, check_each);
             },
             Travel::Up(_)|Travel::Down(_) => {
-                update_crossings(x, &mut horizontals, y + 1, current.1 - 1, |path_distance: i32, on: i32| {
+                let check_each = |path_distance: i32, on: i32| {
                     let pos = (x, on);
-                    if x == -1435 && on == 2474 {
-                        println!("FOUND! {} + {}", distance, path_distance);
-                    }
                     if intersections.contains(&pos) {
-                        time_distances.entry(pos).or_insert(distance + path_distance);
-                        println!("Found intersection after {} at {:?}", distance + path_distance, (x, on));
+                        time_distances.entry(pos).or_insert_with(|| {
+                            distance + path_distance
+                        });
                     }
-                });
+                };
+                update_crossings(x, &mut horizontals, y, current.1, check_each);
             },
         }
 
@@ -128,11 +129,17 @@ fn get_all_edge_data(path: &Vec<Travel>, intersections: &Vec<(i32, i32)>) -> (
 fn update_crossings<F: FnMut(i32, i32)>(at: i32, track: &mut HashMap<i32, HashSet<i32>>, from: i32, to: i32,
     mut check_each: F) {
 
-    for i in abs_range_inclusive(from, to) {
+    let mut path_distance = 0;
+    let mut range = abs_range_inclusive(from, to).skip(1).peekable();
+    while let Some(i) = range.next() {
+        if range.peek().is_none() {
+            break;
+        }
+        path_distance += 1;
         track.entry(i)
             .or_insert_with(|| HashSet::new())
             .insert(at);
-        check_each((from.max(to) - i).abs(), i);
+        check_each(path_distance, i);
     }
 }
 
@@ -177,10 +184,10 @@ fn get_intersections(
     intersections
 }
 
-fn abs_range_inclusive(i1: i32, i2: i32)-> RangeInclusive<i32> {
+fn abs_range_inclusive(i1: i32, i2: i32)-> Box<dyn Iterator<Item = i32>> {
     if i1 > i2 {
-        i2..=i1
+        Box::new((i2..=i1).rev())
     } else {
-        i1..=i2
+        Box::new(i1..=i2)
     }
 }
