682fe6941c9f341650b9e853452b6627c714930d
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2021-10-30 16:19:39 +0100

    VM improvements to start supporting modes

--- src/aoc2019/vm/mod.rs
+++ src/aoc2019/vm/mod.rs
@@ -14,12 +14,36 @@ mod ops {
     pub const HALT: i32 = 99;
 }
 
+#[derive(Debug, PartialEq, Eq)]
+enum Mode {
+    Positional(i32),
+    Immediate(i32),
+}
+
+impl Mode {
+    fn resolve(&self, memory: &Vec<i32>) -> Option<i32>  {
+        match self {
+            Mode::Positional(i) => {
+                if (*i < 0) || (*i as usize) >= memory.len() {
+                    None
+                } else {
+                    Some(memory[*i as usize])
+                }
+            }
+            Mode::Immediate(i) => Some(*i)
+        }
+    }
+}
+
 pub struct Computer {
     position: usize,
     program: Vec<i32>,
 }
 
+type Args = (Option<i32>, i32, i32, i32);
+
 impl Computer {
+    // TODO return Result
     pub fn new(program: Vec<i32>) -> Self {
         Computer {
             position: 0,
@@ -30,6 +54,11 @@ impl Computer {
     pub fn run(&mut self) -> i32 {
         loop {
             let (opcode, a, b, c) = self.get_args();
+
+            if let None = opcode {
+                return -1;
+            }
+            let opcode = opcode.unwrap();
 
             match opcode {
                 ops::ADD | ops::MUL => {
@@ -54,9 +83,19 @@ impl Computer {
         }
     }
 
-    fn get_args(&self) -> (i32, i32, i32, i32) {
+    fn get_args(&self) -> Args {
+        let raw_op = self.safe_vec_get(0);
+        let opcode = if self.position >= self.mem_size() {
+            None
+        } else {
+            Some(raw_op % 100)
+        };
+    
+
+
+
         return (
-            self.safe_vec_get(0),
+            opcode,
             self.safe_vec_get(1),
             self.safe_vec_get(2),
             self.safe_vec_get(3),
@@ -79,6 +118,10 @@ impl Computer {
 
     fn get_mem(&self, offset: i32) -> i32 {
         self.program[offset as usize]
+    }
+
+    fn mem_size(&self) -> usize {
+        self.program.len()
     }
 
     fn set_mem(&mut self, offset: i32, value: i32) {
