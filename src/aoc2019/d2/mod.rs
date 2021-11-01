1362b679b6446fec5d7eac584be652d510c33786
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2021-11-01 09:01:05 +0000

    Massive VM refactor ahead of d5

--- src/aoc2019/d2/mod.rs
+++ src/aoc2019/d2/mod.rs
@@ -1,25 +1,28 @@
 use crate::vm;
-use crate::vm::Computer;
+use crate::vm::{Computer, EResult};
 
 pub fn solve(input1: String, _: String, _: &[String]) {
     let program: Vec<i32> = vm::read_program(input1);
 
-    let part1 = run_with(&program, (12, 2));
-    println!("Program execution ended with result: {}", part1);
+    match run_with(&program, (12, 2)) {
+        Ok(res) => println!("Program execution ended with result: {}", res),
+        Err(e) => panic!("{}", e),
+    }
 
     for noun in 0..=255 {
         for verb in 0..=255 {
-            if 19690720 == run_with(&program, (noun, verb)) {
-                println!(
+            match run_with(&program, (noun, verb)) {
+                Ok(19690720) => println!(
                     "Program execution ended with program code: {}",
                     noun * 100 + verb
-                );
+                ),
+                _ => {}
             }
         }
     }
 }
 
-fn run_with(program: &Vec<i32>, init: (i32, i32)) -> i32 {
+fn run_with(program: &Vec<i32>, init: (i32, i32)) -> EResult<i32> {
     let mut copy = program.clone();
     copy[1] = init.0;
     copy[2] = init.1;
--- src/aoc2019/vm/mod.rs
+++ src/aoc2019/vm/mod.rs
@@ -1,4 +1,5 @@
 use crate::utils;
+use std::fmt;
 
 pub fn read_program(input: String) -> Vec<i32> {
     utils::read_file_lines(&input)
@@ -14,33 +15,82 @@ mod ops {
     pub const HALT: i32 = 99;
 }
 
+#[derive(Debug, Clone, Copy)]
+enum ProgramError {
+    InvalidOpcode(i32),
+    InvalidMode(i32, u32),
+    InvalidMemory(i32),
+}
+
+impl fmt::Display for ProgramError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let msg = match self {
+            ProgramError::InvalidOpcode(i) => format!("Invalid opcode {}", i),
+            ProgramError::InvalidMode(mode, i) => format!("Invalid mode {} at position {}", mode, i),
+            ProgramError::InvalidMemory(i) => format!("Out of range memory address {}", i),
+        };
+        write!(f, "{}", msg)
+    }
+}
+
+pub struct ExecutionError(usize, ProgramError);
+
+pub type EResult<T> = Result<T, ExecutionError>;
+
+impl fmt::Display for ExecutionError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Execution error at instruction {:#x}: {}", self.0, self.1)
+    }
+}
+
 #[derive(Debug, PartialEq, Eq)]
 enum Mode {
-    Positional(i32),
-    Immediate(i32),
+    Positional,
+    Immediate,
 }
 
 impl Mode {
-    fn resolve(&self, memory: &Vec<i32>) -> Option<i32>  {
+    fn of(modes: i32, pos: u32) -> PResult<Self> {
+        let i = (modes / 10_i32.pow(pos)) % 10;
+        match i {
+            0 => Ok(Mode::Positional),
+            1 => Ok(Mode::Immediate),
+            _ => Err(ProgramError::InvalidMode(modes, pos)),
+        }
+    }
+
+    fn resolve(&self, i:  i32, memory: &Vec<i32>) -> PResult<i32>  {
         match self {
-            Mode::Positional(i) => {
-                if (*i < 0) || (*i as usize) >= memory.len() {
-                    None
+            Mode::Positional => {
+                if (i < 0) || (i as usize) >= memory.len() {
+                    Err(ProgramError::InvalidMemory(i))
                 } else {
-                    Some(memory[*i as usize])
+                    Ok(memory[i as usize])
                 }
             }
-            Mode::Immediate(i) => Some(*i)
+            Mode::Immediate => Ok(i)
         }
     }
 }
 
+type PResult<T> = Result<T, ProgramError>;
+
+struct Arg(Mode, PResult<i32>);
+
+impl Arg {
+    fn resolve(&self, mem: &Vec<i32>) -> PResult<i32> {
+        self.0.resolve(self.1?, mem)
+    }
+}
+
+type Args = (
+    i32,
+    (Arg, Arg, Arg)
+);
 pub struct Computer {
     position: usize,
     program: Vec<i32>,
 }
-
-type Args = (Option<i32>, i32, i32, i32);
 
 impl Computer {
     // TODO return Result
@@ -51,68 +101,87 @@ impl Computer {
         }
     }
 
-    pub fn run(&mut self) -> i32 {
+    pub fn run(&mut self) -> EResult<i32> {
         loop {
-            let (opcode, a, b, c) = self.get_args();
-
-            if let None = opcode {
-                return -1;
-            }
-            let opcode = opcode.unwrap();
-
-            match opcode {
-                ops::ADD | ops::MUL => {
-                    if !self.mem_safe(a) || !self.mem_safe(b) || !self.mem_safe(c) {
-                        return -1;
-                    }
-                }
-                _ => {}
+            let args = self.get_args();
+            if let Err(e) = args {
+                return Err(ExecutionError(self.position, e));
             }
 
-            match opcode {
-                ops::ADD => self.set_mem(c, self.get_mem(a) + self.get_mem(b)),
-                ops::MUL => self.set_mem(c, self.get_mem(a) * self.get_mem(b)),
-                ops::HALT => return self.program[0],
-                _ => panic!("Invalid opcode found: {}", opcode),
-            };
+            let args = args.unwrap();
+
+            let opcode = args.0;
+
+            if ops::HALT == opcode {
+                return Ok(self.program[0])
+            }
+
+            let opargs = args.1;
+            if let Err(err) = self.apply_opcode(opcode, opargs) {
+                return Err(ExecutionError(self.position, err) )
+            }
 
             match opcode {
                 ops::ADD | ops::MUL => self.position += 4,
-                _ => panic!("Invalid opcode found: {}", opcode),
+                _ => panic!("Invalid opcode was not validated: {}", opcode),
             }
         }
     }
 
-    fn get_args(&self) -> Args {
-        let raw_op = self.safe_vec_get(0);
-        let opcode = if self.position >= self.mem_size() {
-            None
-        } else {
-            Some(raw_op % 100)
-        };
-    
+    fn apply_opcode(&mut self, opcode: i32, opargs: (Arg, Arg, Arg)) -> PResult<()> {
+        let (a, b, c) = opargs;
 
+        match opcode {
+            ops::ADD => self.set_mem_arg(c,self.get_mem_arg(a)? + self.get_mem_arg(b)?),
+            ops::MUL => self.set_mem_arg(c, self.get_mem_arg(a)? * self.get_mem_arg(b)?),
+            _ => Err(ProgramError::InvalidOpcode(opcode)),
+        }
+    }
 
+    fn get_args(&self) -> PResult<Args> {
+        let raw_op = self.safe_vec_get(0)?;
 
-        return (
-            opcode,
-            self.safe_vec_get(1),
-            self.safe_vec_get(2),
-            self.safe_vec_get(3),
+        let opcode = raw_op % 100;
+        // TODO validate opcode
+
+        let modes =  raw_op / 100;
+
+        let arg_modes = (
+            Mode::of(modes, 0)?,
+            Mode::of(modes, 1)?,
+            Mode::of(modes, 2)?,
         );
+
+        Ok((
+            opcode,
+            (
+                self.as_arg(arg_modes.0, 1),
+                self.as_arg(arg_modes.1, 2),
+                self.as_arg(arg_modes.2, 3),
+            )
+        ))
     }
 
-    fn mem_safe(&self, offset: i32) -> bool {
-        self.program.len() > (offset as usize)
+    fn as_arg(&self, mode: Mode, offset: usize) -> Arg {
+        let location = self.position + offset;
+        Arg(mode, if self.mem_safe(location) {
+            Ok(location as i32)
+        } else {
+            Err(ProgramError::InvalidMemory(location as i32))
+        })
     }
 
-    fn safe_vec_get(&self, offset: usize) -> i32 {
+    fn mem_safe(&self, offset: usize) -> bool {
+        self.program.len() > offset
+    }
+
+    fn safe_vec_get(&self, offset: usize) -> PResult<i32> {
         let location = self.position + offset;
 
-        if !self.mem_safe(location as i32) {
-            -1
+        if !self.mem_safe(location) {
+            Err(ProgramError::InvalidMemory(location as i32))
         } else {
-            self.program[location]
+            Ok(self.program[location])
         }
     }
 
@@ -120,11 +189,21 @@ impl Computer {
         self.program[offset as usize]
     }
 
-    fn mem_size(&self) -> usize {
-        self.program.len()
+    fn get_mem_arg(&self, arg: Arg) -> PResult<i32> {
+        let location = arg.resolve(&self.program)?;
+
+        if !self.mem_safe(location as usize) {
+            return Err(ProgramError::InvalidMemory(location as i32))
+        }
+
+        Ok(self.get_mem(location))
     }
 
     fn set_mem(&mut self, offset: i32, value: i32) {
         self.program[offset as usize] = value
+    }
+
+    fn set_mem_arg(&mut self, arg: Arg, val: i32) -> PResult<()> {
+        Ok(self.set_mem(arg.resolve(&self.program)?, val))
     }
 }
